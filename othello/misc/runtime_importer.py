c29cf0ca5b7ba0293dad20a99cb6ac9ef9b24adb
HeavenVolkoff <vasconcellos.dev@gmail.com>
2019-11-06 14:44:49 -0300

    Reverte mudanças no import runtime feitas no commit f6fa31c  - Implementa import de packages

--- othello/misc/runtime_importer.py
+++ othello/misc/runtime_importer.py
@@ -1,10 +1,9 @@
 # Internal
+import sys
 import typing as T
 from os import path
-from glob import iglob
 from inspect import isclass
-from pkgutil import ModuleInfo
-from itertools import chain
+from pkgutil import ModuleInfo, walk_packages
 from importlib.util import module_from_spec
 from importlib.machinery import FileFinder, SourceFileLoader
 
@@ -16,44 +15,50 @@ from ..models.board import Board
 def available_players(player_paths: T.Optional[T.Sequence[str]] = None) -> T.Sequence[ModuleInfo]:
     import othello
 
-    module_path: str = othello.__path__  # type: ignore  # mypy issue #1422
-    player_paths = player_paths or tuple()
-    return tuple(
-        ModuleInfo(
-            FileFinder(path.dirname(file_path), (SourceFileLoader, (".py",))),
-            path.splitext(path.basename(file_path))[0],
-            False,
-        )
-        for file_path in chain(
-            *(
-                iglob(path.join(dir_path, "**", "*.py"), recursive=True)
-                for dir_path in chain(
-                    (path.join(p, "models", "players") for p in module_path),
-                    (dir_path for dir_path in player_paths if path.isdir(dir_path)),
-                )
-            ),
-            (
-                file_path
-                for file_path in player_paths
-                if path.isfile(file_path) and file_path.endswith(".py")
-            ),
-        )
-        if not path.basename(file_path).startswith("_")
+    module_paths = set(player_paths) or set()
+    othello_path: str = othello.__path__  # type: ignore  # mypy issue #1422
+    package_paths = {
+        *(path.join(p, "models", "players") for p in othello_path),
+        *(dir_path for dir_path in module_paths if path.isdir(dir_path)),
+    }
+
+    module_paths -= package_paths
+    return (
+        *walk_packages(package_paths),
+        *(
+            ModuleInfo(
+                FileFinder(path.dirname(module_path), (SourceFileLoader, (".py",))),
+                path.splitext(path.basename(module_path))[0],
+                False,
+            )
+            for module_path in module_paths
+            if (
+                path.isfile(module_path)
+                and module_path.endswith(".py")
+                and not path.basename(module_path).startswith("_")
+            )
+        ),
     )
 
 
 def import_player(player_importer: ModuleInfo) -> T.Type[PlayerProtocol]:
-    importer, module_name, _ = player_importer
+    loader, module_name, is_package = player_importer
 
-    module_spec = importer.find_spec(module_name)
+    if is_package:
+        sys.path.append(path.join(loader.path))
+
+    module_spec = loader.find_spec(module_name)
     if module_spec is None:
-        raise ImportError(f"Failed to import player class from module: {module_name}")
+        raise ImportError(
+            f"Failed to import player class from {'package' if is_package else 'module'}: "
+            f"{module_name}"
+        )
 
     module = module_from_spec(module_spec)
     module_spec.loader.exec_module(module)
-    module_all = getattr(module, "__all__", None)
-    symbols = tuple(symbol for symbol in dir(module) if symbol[0] != "_")
 
+    symbols = tuple(symbol for symbol in dir(module) if symbol[0] != "_")
+    module_all = getattr(module, "__all__", None)
     if module_all:
         symbols = tuple(symbol for symbol in symbols if symbol in module_all)
 
@@ -71,7 +76,4 @@ def import_player(player_importer: ModuleInfo) -> T.Type[PlayerProtocol]:
     return player_cls
 
 
-__all__ = (
-    "import_player",
-    "available_players",
-)
+__all__ = ("import_player", "available_players")
